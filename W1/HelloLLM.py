25bd447806c500f81bed0347b98fce1bd4d0b106
Xavier Ochoa <xavier.ochoa@gmail.com>
2024-08-30 16:37:34 -0400

    first commit

---
+++ W1/HelloLLM.py
@@ -0,0 +1,56 @@
+import os
+import getpass
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+#os.environ["OPENAI_API_KEY"] = getpass.getpass()
+#os.environ["GROQ_API_KEY"] = getpass.getpass()
+#os.environ["GOOGLE_API_KEY"] = getpass.getpass()
+
+#model = ChatOpenAI(model="gpt-3.5-turbo")
+#model = ChatGroq(model="mixtral-8x7b-32768")
+#model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+model = ChatOpenAI(base_url="http://localhost:1234/v1", api_key="not-needed", streaming=True)
+
+prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """"You are a history teacher that is eager to help students understand the story behind the history.""",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+
+chain = prompt | model
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+with_message_history = RunnableWithMessageHistory(chain, get_session_history)
+
+config = {"configurable": {"session_id": "session1"}}
+
+question="I want to know about the battle of Waterloo"
+print(question)
+for r in with_message_history.stream(
+    [question],
+    config=config,
+):
+    print(r.content, end="")
+
+question="Why they fought?"
+print(question)
+for r in with_message_history.stream(
+    [question],
+    config=config,
+):
+    print(r.content, end="")
---
+++ W2/app.py
@@ -0,0 +1,53 @@
+from langchain_groq import ChatGroq
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+import os
+import chainlit as cl
+
+store = {}
+config = {"configurable": {"session_id": "session1"}}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+@cl.on_chat_start
+async def on_chat_start():
+    os.environ["GROQ_API_KEY"] = "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"
+    model = ChatGroq(model="mixtral-8x7b-32768", streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """"Act as knowledgeable and gentle algebra tutor. Students will ask about how to solve a given equation, you should guide them one step at a time 
+            without providing a direct answers, but using the socratic method to help them get closer to the answer. Do not answer everything in just one answer.  Always remember the original equation to provide 
+            correct guidance.""",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    chain = prompt | model | StrOutputParser()
+    with_message_history = RunnableWithMessageHistory(chain, get_session_history)
+    cl.user_session.set("runnable", with_message_history)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()], session_id="session1"),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
+
