0c1daf401c624bebdaca06a5d65c205f802bbe74
Xavier Ochoa <xavier.ochoa@gmail.com>
2024-09-24 17:32:14 -0400

    W4 update

--- W4/app.py
+++ W4/app.py
@@ -1,43 +1,70 @@
 import pandas as pd
-import sqlite3
 import chainlit as cl
-from openai import AsyncOpenAI
-from pandasai import SmartDataframe
-from langchain_community.llms import Ollama
+from pandasai import Agent
 from langchain_groq.chat_models import ChatGroq 
-import sqlite3
-import os
 
-llm = ChatGroq(model_name="llama3-70b-8192", api_key = os.environ["GROQ_API_KEY"])
 
 @cl.on_chat_start
-def start_chat():
-    # Set initial message history
-    cl.user_session.set(
-        "message_history",
-        [{"role": "system", "content": "You are a helpful assistant."}],
-    )
+async def start_chat():
+    llm = ChatGroq(model_name="llama3-70b-8192")
+    cl.user_session.set("llm", llm)
+
+     # Sending an image with the local file path
+    elements = [
+        cl.Image(name="image1", display="inline", path="./robot.jpeg")
+        ]
+    await cl.Message(content="Hello there, Welcome to Data Analysis Agent!", elements=elements).send()
+
+    files = None
+
+    #### Load external data
+    while files is None:
+        files = await cl.AskFileMessage(
+            content="Please upload a csv file to begin!", 
+            accept=["text/csv"],
+            max_size_mb= 100,
+            timeout = 180,
+        ).send()
+
+    # load the csv data and store in user_session
+    file = files[0]
+
+    msg = cl.Message(content=f"Processing `{file.name}`...")
+    await msg.send()
+
+    # Read csv file with pandas
+    #csv_file = io.BytesIO(file.content)
+    df = pd.read_csv(file.path, encoding="utf-8")
+
+    #### Load internal Excel data
+    ## Load data
+    #df = pd.read_excel('schoolData.xlsx')
+    
+    ### Load internal CSV data
+    #df = pd.read_csv('data.csv')
+
+    dataAgent = Agent(df, config={"llm": llm})
+    # creating user session to store data
+    cl.user_session.set('dataAgent', dataAgent)
+
+    # Send response back to user
+    # Let the user know that the system is ready
+    msg.content = f"Processing `{file.name}` done. You can now ask questions!"
+    await msg.update()
+    
+
 
 @cl.on_message
 async def main(message: cl.Message):
-    # Retrieve message history
-    message_history = cl.user_session.get("message_history")
-    message_history.append({"role": "user", "content": message.content})
-
-    # Load data
-    df = pd.read_excel('schoolData.xlsx')
-    # df = pd.read_csv('data.csv')
-
-
-    df = SmartDataframe(df, config={"llm": llm})
+  
+    agent = cl.user_session.get('dataAgent')
     
     question = message.content
-    response = df.chat(question)
-    
+    response = agent.chat(question)
+
     msg= None
     if isinstance(response, str) and ".png" in response:
         image = cl.Image(path=response, name="image1", display="inline")
-
         # Attach the image to the message
         msg = cl.Message(content="Here is the chart:", elements=[image])
     else:
@@ -45,6 +72,5 @@ async def main(message: cl.Message):
     
     await msg.send()
 
-    # Update message history and send final message
-    message_history.append({"role": "assistant", "content": msg.content})
+    # Send final message
     await msg.update()
